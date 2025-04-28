dd0e82925a661101dc10f14b7e9b4db360bbf088
Daniela Loustalot Knapp <dloustal@student.codam.nl>
2025-04-28 11:42:35 +0200

    Edited tokenizing of env_vars

---
+++ traversal/src/var_expansion.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   var_expansion.c                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dloustal <dloustal@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/04/28 09:49:04 by dloustal      #+#    #+#                 */
+/*   Updated: 2025/04/28 10:14:25 by dloustal      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "traversal.h"
+
+void	expand_var(t_token_node *node, t_vars *vars)
+{
+	if (!node || !vars)
+		return ;
+	
+}
