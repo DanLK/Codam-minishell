61d817df77620caf697cd681d3aaa1b04dd68e6a
Daniela Loustalot Knapp <dloustal@student.codam.nl>
2025-06-18 17:57:18 +0200

    Refactored long functions for variable expansion

---
+++ minishell/src/string_exp_len.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   string_exp_len.c                                   :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dloustal <dloustal@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/06/18 16:44:59 by dloustal      #+#    #+#                 */
+/*   Updated: 2025/06/18 17:35:37 by dloustal      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static void	handle_quotes(size_t *len, t_exp *vexp)
+{
+	if (vexp->s[vexp->i] == '\'')
+	{
+		vexp->in_single = !vexp->in_single;
+		if (vexp->in_double)
+			(*len)++;
+		vexp->i++;
+	}
+	else if (vexp->s[vexp->i] == '\"')
+	{
+		vexp->in_double = !vexp->in_double;
+		if (vexp->in_single)
+			(*len)++;
+		vexp->i++;
+	}
+}
+
+static void	advance_var_size(size_t *len, t_exp *vexp, t_vars *vars)
+{
+	char	*var_name;
+	t_vars	*var;
+
+	var_name = get_var_name(vexp->s, vexp->i - 1);
+	var = find_vars(vars, var_name);
+	if (var)
+		*len += ft_strlen(var->value);
+	free(var_name);
+}
+
+static void	handle_vars(size_t *len, t_exp *vexp, t_vars *vars, t_info *info)
+{
+	char	*get_len;
+
+	if (!vexp->in_single && vexp->s[vexp->i] == '$')
+	{
+		vexp->i++;
+		if (vexp->s[vexp->i] && vexp->s[vexp->i] == '?')
+		{
+			get_len = ft_itoa(info->last_return_code);
+			*len += ft_strlen(get_len);
+			vexp->i++;
+			free(get_len);
+		}
+		else if (vexp->s[vexp->i] && (ft_isalpha(vexp->s[vexp->i])
+				|| vexp->s[vexp->i] == '_')) // I think ft_isalpha
+		{
+			advance_var_size(len, vexp, vars);
+			while (vexp->s[vexp->i] && (ft_isalnum(vexp->s[vexp->i])
+					|| vexp->s[vexp->i] == '_'))
+				vexp->i++;
+		}
+		else
+			(*len)++;
+	}
+}
+
+/*****************************************************************************
+ * Calculates the length of the expected expanded string
+ * 
+ * It expands everywhere but in single quotes
+ * Ignores single and double quotes in the counting
+******************************************************************************/
+int	qstr_exp_len(char *s, t_vars *vars, t_info *info)
+{
+	size_t	len;
+	t_exp	*vexp;
+
+	len = 0;
+	vexp = init_var_exp(s);
+	if (!vexp)
+		return (0);
+	if (!s)
+		return (0);
+	while (s[vexp->i])
+	{
+		if (s[vexp->i] == '\'' || s[vexp->i] == '\"')
+			handle_quotes(&len, vexp);
+		else if (!vexp->in_single && s[vexp->i] == '$')
+			handle_vars(&len, vexp, vars, info);
+		else
+		{
+			len++;
+			vexp->i++;
+		}
+	}
+	free(vexp->s);
+	free(vexp);
+	return (len);
+}
--- minishell/src/var_expansion.c
+++ minishell/src/var_expansion.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   var_expansion.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rojornod <rojornod@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/28 09:49:04 by dloustal          #+#    #+#             */
-/*   Updated: 2025/06/17 15:16:49 by rojornod         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   var_expansion.c                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dloustal <dloustal@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/04/28 09:49:04 by dloustal      #+#    #+#                 */
+/*   Updated: 2025/06/18 17:34:40 by dloustal      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,212 +77,6 @@ void	expand_redir_files(t_redir_node **red_head, t_vars *v, t_info *info)
 	}
 }
 
-/*****************************************************************************
- * Exchanges the environment variable for its value in a given node
- * Assumes the token type of the node is TKN_ENV_VAR
-******************************************************************************/
-// void	expand_envvar(t_token_node *node, t_vars *vars)
-// {
-// 	t_vars	*var;
-// 	char	*old_lexeme;
-
-// 	if (!node || !vars)
-// 		return ;
-// 	var = find_vars(vars, node->token->lexeme);
-// 	old_lexeme = node->token->lexeme;
-// 	free(old_lexeme);
-// 	if (var)
-// 		node->token->lexeme = ft_strdup(var->value);
-// 	else
-// 		node->token->lexeme = ft_strdup("");
-// 		// perror("Variable not found"); //This is a syntax error and should stop and clean everything
-// 		// return ; //Clear memory
-// 		// // !!!!!!!!!!!!!!!!!! It's actually not a syntax error, it just prints nothing
-// 	node->token->type = TKN_WORD;
-// }
-
-/*****************************************************************************
- * Exchanges the $? for its value in a given node
- * Assumes the token type of the node is TKN_EXIT_STATUS
-******************************************************************************/
-// void	expand_exitstatus(t_token_node *node, t_info *info)
-// {
-// 	int		exit_status;
-// 	char	*old_lexeme;
-
-// 	if (!node || !info)
-// 		return ;
-// 	exit_status = info->last_return_code;
-// 	old_lexeme = node->token->lexeme;
-// 	free(old_lexeme);
-// 	node->token->lexeme = ft_itoa(exit_status);
-// 	node->token->type = TKN_WORD;
-// }
-
-/*****************************************************************************
- * Builds a new string by expanding variables and exit status 
- * 
- * Expands everything that is not in single quotes
-******************************************************************************/
-char	*expand_qstring(char *s, t_vars *vars, t_info *info)
-{
-	char	*result;
-	int		len;
-	bool	in_single;
-	bool	in_double;
-	int		i;
-	int		res_i;
-	char	*var_name;
-	t_vars	*var;
-
-	if (!s)
-		return (NULL);
-	len = qstr_exp_len(s, vars, info);
-	result = malloc((len + 1) * sizeof(char));
-	if (!result)
-		return (NULL);
-	in_single = false;
-	in_double = false;
-	i = 0;
-	res_i = 0;
-	while (s[i])
-	{
-		if (s[i] == '\'')
-		{
-			if (in_double)
-				result[res_i++] = s[i];
-			else
-				in_single = !in_single;
-			i++;
-			continue ;
-		}
-		else if (s[i] == '\"')
-		{
-			if (in_single)
-				result[res_i++] = s[i];
-			else
-				in_double = !in_double;
-			i++;
-		}
-		else if (!in_single && s[i] == '$')
-		{
-			i++;
-			if (s[i] == '?')
-			{
-				put_exitstatus(&result, info, &res_i);
-				i++;
-			}
-			else if (s[i] && (ft_isalnum(s[i]) || s[i] == '_'))
-			{
-				var_name = get_var_name(s, i - 1);
-				var = find_vars(vars, var_name);
-				if (var)
-					put_var(&result, var->value, &res_i);
-				else
-					put_var(&result, "", &res_i);
-				i += ft_strlen(var_name);
-				free(var_name);
-			}
-			else
-				result[res_i++] = '$';
-		}
-		else
-			result[res_i++] = s[i++];
-	}
-	result[res_i] = '\0';
-	return (result);
-}
-
-// /*****************************************************************************
-//  * Expands all variables in a string while respecting the rest of the characters
-//  * Returns a new string with all the vars replaced
-// ******************************************************************************/
-// char	*expand_string(char *string, t_vars *vars)
-// {
-// 	int		i;
-// 	char	*result;
-// 	char	*tmp;
-// 	int		len;
-
-// 	if (!string || !vars)
-// 		return (NULL);
-// 	if (!ft_strnstr(string, "$", ft_strlen(string)))
-// 		return (ft_strdup(string));
-// 	i = get_position(string, '$');
-// 	result = ft_strdup(string);
-// 	if (!result)
-// 		return (NULL);
-// 	len = (int)ft_strlen(string);
-// 	while (i < len)
-// 	{
-// 		tmp = result;
-// 		result = expand_one_string(tmp, vars);
-// 		free(tmp);
-// 		if (!result)
-// 			return (NULL);
-// 		i = get_position(result, '$');
-// 		len = (int)ft_strlen(result);
-// 	}
-// 	return (result);
-// }
-
-// /*****************************************************************************
-// * Expands a variable in a double quoted string
-// * Returns a string with the var replaced
-// * 
-// * aux[0] = start
-// * aux[1] = var_name
-// * aux[2] = start + var value
-// * aux[3] = rest
-// * 
-// ******************************************************************************/
-// char	*expand_one_string(char *string, t_vars *vars)
-// {
-// 	int		i;
-// 	char	*result;
-// 	char	**aux;
-// 	t_vars	*var;
-
-// 	i = get_position(string, '$');
-// 	aux = malloc(5 * sizeof(char *));
-// 	if (!aux)
-// 		return (NULL);
-// 	aux[0] = ft_substr(string, 0, i);
-// 	aux[1] = get_var_name(string, i);
-// 	var = find_vars(vars, aux[1]);
-// 	if (!var || !var->value)
-// 		aux[2] = ft_strjoin(aux[0], "");
-// 	else
-// 		aux[2] = ft_strjoin(aux[0], var->value);
-// 	aux[3] = ft_substr(string, i + 1 + ft_strlen(aux[1]),
-// 			ft_strlen(string)- i - 1 - ft_strlen(aux[1]));
-// 	aux[4] = NULL;
-// 	result = ft_strjoin(aux[2], aux[3]);
-// 	clear_array(aux);
-// 	return (result);
-// }
-
-// char	*get_start_trim_quotes(char *string, size_t len)
-// {
-// 	char	*result;
-// 	int		i;
-// 	int		j;
-
-// 	result = malloc(len);
-// 	if (!result)
-// 		return (NULL);
-// 	i = 0;
-// 	j = 0;
-// 	while (string[i])
-// 	{
-// 		if (string[i] != '\"')
-// 			result[j++] = string[i];
-// 		i++;
-// 	}
-// 	result[j] = '\0';
-// 	return (result);
-// }
-
 char	*remove_quotes(char *string)
 {
 	char	*result;
@@ -303,3 +97,77 @@ char	*remove_quotes(char *string)
 	result[j] = '\0';
 	return (result);
 }
+
+// /************************************************************************
+//  * Builds a new string by expanding variables and exit status 
+//  * 
+//  * Expands everything that is not in single quotes
+// *************************************************************************/
+// char	*expand_qstring_cpy(char *s, t_vars *vars, t_info *info)
+// {
+// 	char	*result;
+// 	int		len;
+// 	bool	in_single;
+// 	bool	in_double;
+// 	int		i;
+// 	int		res_i;
+// 	char	*var_name;
+// 	t_vars	*var;
+
+// 	if (!s)
+// 		return (NULL);
+// 	len = qstr_exp_len(s, vars, info);
+// 	result = malloc((len + 1) * sizeof(char));
+// 	if (!result)
+// 		return (NULL);
+// 	in_single = false;
+// 	in_double = false;
+// 	i = 0;
+// 	res_i = 0;
+// 	while (s[i])
+// 	{
+// 		if (s[i] == '\'')
+// 		{
+// 			if (in_double)
+// 				result[res_i++] = s[i];
+// 			else
+// 				in_single = !in_single;
+// 			i++;
+// 			continue ;
+// 		}
+// 		else if (s[i] == '\"')
+// 		{
+// 			if (in_single)
+// 				result[res_i++] = s[i];
+// 			else
+// 				in_double = !in_double;
+// 			i++;
+// 		}
+// 		else if (!in_single && s[i] == '$')
+// 		{
+// 			i++;
+// 			if (s[i] == '?')
+// 			{
+// 				put_exitstatus(&result, info, &res_i);
+// 				i++;
+// 			}
+// 			else if (s[i] && (ft_isalnum(s[i]) || s[i] == '_'))
+// 			{
+// 				var_name = get_var_name(s, i - 1);
+// 				var = find_vars(vars, var_name);
+// 				if (var)
+// 					put_var(&result, var->value, &res_i);
+// 				else
+// 					put_var(&result, "", &res_i);
+// 				i += ft_strlen(var_name);
+// 				free(var_name);
+// 			}
+// 			else
+// 				result[res_i++] = '$';
+// 		}
+// 		else
+// 			result[res_i++] = s[i++];
+// 	}
+// 	result[res_i] = '\0';
+// 	return (result);
+// }
